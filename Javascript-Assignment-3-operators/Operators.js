34a239232c16f9fdf9269a5de44fd2570c0d573f
PranavGithubUser <pranavkavade9@gmail.com>
2025-01-30 19:43:30 +0530

    Assigment DOM added

---
+++ Javascript-Assignment-3-operators/Operators.js
@@ -0,0 +1,25 @@
+let num1 = 100
+let num2 = 2
+console.log("\n.............Arithemetic Operator..........\n");
+
+console.log(`Addition: ${num1 + num2}`);
+console.log(`Substraction: ${num1 - num2}`);
+console.log(`Multiplication: ${num1 * num2}`);
+console.log(`Division: ${num1 / num2}`);
+console.log(`Modulues: ${num1 % num2}`);
+
+console.log("\n.............Comparision Operator..........\n");
+
+console.log(`Is First number is greater than second :${num1 > num2}`);
+console.log(`Is First number is equal to  second :${num1 === num2}`);
+console.log(`Is First number is less than  second :${num1 < num2}`);
+
+console.log("\n.............Logical Operator..........\n");
+let mathScore = 95;
+let scienceScore = 90;
+
+let isEligible = (mathScore >= 90) && (scienceScore >= 85);
+console.log(`Scholarship Eligibility:${isEligible}`);
+
+console.log("\n.............Assignment Operator..........\n");
+
